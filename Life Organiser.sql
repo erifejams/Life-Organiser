907851bcbb8b69bc38fe7f29541875af5d6c8b85
Erifeoluwa Jamgbadi <44726422+erifejams@users.noreply.github.com>
2021-08-30 23:36:16 +0100

    Creating database
    Created the database and added tables

---
+++ Life Organiser.sql
@@ -0,0 +1,29 @@
+-- CREATED BY : Erifeoluwa Jamgbadi --
+
+-- Creating the database called life organiser --
+CREATE DATABASE lifeOrganiser;
+
+-- CREATING TABLES --
+
+-- THESE TABLES ARE HOBBIES THAT I AM INTERESTED IN
+CREATE TABLE Movies (
+	movieId BIGINT(50) NOT NULL,
+    genre VARCHAR(100),
+    langauge VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE TVSeries(
+	
+);
+
+CREATE TABLE Music(
+	
+);
+
+CREATE TABLE YouTube(
+	
+);
+
+CREATE TABLE Coding(
+	
+);
